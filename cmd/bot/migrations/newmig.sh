a5645df582363f90fe889deca61ae2c1936ab32e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-06-20 23:10:23 +0200

    freshen up migration script

--- cmd/bot/migrations/newmig.sh
+++ cmd/bot/migrations/newmig.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-RELATIVE_PATH=$(dirname $0)
+set -e
 
-if [ -z $1 ]; then
+script_path=$(dirname "$0")
+description=$1
+
+if [ -z "$description" ]; then
     echo "usage: $0 description-of-migration"
     exit 1
 fi
 
-touch "$RELATIVE_PATH/$(date --utc '+%s')-$1.sql"
+touch "$script_path/$(date --utc '+%s')-$description.sql"
