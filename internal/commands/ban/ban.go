1f2794c4ccfaffa34b5e641dd5ed9cffc3ff7008
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-06-20 23:03:44 +0200

    add missing import

--- internal/commands/ban/ban.go
+++ internal/commands/ban/ban.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/pajbot/basecommand"
 	"github.com/pajbot/pajbot2-discord/internal/config"
 	"github.com/pajbot/pajbot2-discord/pkg"
 	"github.com/pajbot/pajbot2-discord/pkg/commands"
