057abeaa0522ae3fef74fbdfd21a6e9101412ebb
Aitor Martin <sakurazaki.setsunita@gmail.com>
2019-08-03 15:28:06 +0200

    added whereisclown to check last tweet from forsen

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,6 +18,12 @@ var (
 	DSN string
 
 	Token string
+	
+	TwitterUserName				string
+	TwitterConsumerKey 			string
+	TwitterConsumerSecret 		string
+	TwitterAccessToken			string
+	TwitterAccessTokenSecret	string
 
 	AdminRoles []string
 
@@ -32,6 +38,13 @@ func init() {
 	DSN = stringEnv(envName("SQL_DSN"), "postgres:///pajbot2_discord?sslmode=disable")
 
 	Token = mustStringEnv(envName("TOKEN"))
+	
+	// Twitter
+	TwitterUserName = stringEnv(envName("TWITTER_USER_NAME"), nil)
+	TwitterConsumerKey = stringEnv(envName("TWITTER_CONSUMER_KEY"), nil)
+	TwitterConsumerSecret = stringEnv(envName("TWITTER_CONSUMER_SECRET"), nil)
+	TwitterAccessToken = stringEnv(envName("TWITTER_ACCESS_TOKEN"), nil)
+	TwitterAccessTokenSecret = stringEnv(envName("TWITTER_ACCESS_TOKEN_SECRET"), nil)
 
 	// roles
 	MiniModeratorRole = mustStringEnv(envName("MINI_MODERATOR_ROLE"))
