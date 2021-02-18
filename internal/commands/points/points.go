5a699da977002a3e21cae81cdc9bc7d409b9b3eb
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-02-18 23:30:54 +0100

    Update the $points command to use the configurable `pajbot_host` value

--- internal/commands/points/points.go
+++ internal/commands/points/points.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/pajbot/basecommand"
+	"github.com/pajbot/pajbot2-discord/internal/serverconfig"
 	"github.com/pajbot/pajbot2-discord/pkg"
 	"github.com/pajbot/pajbot2-discord/pkg/commands"
 )
@@ -37,6 +38,13 @@ func New() *Command {
 func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []string) (res pkg.CommandResult) {
 	res = pkg.CommandResultUserCooldown
 
+	pajbotHost := serverconfig.Get(m.GuildID, "value:pajbot_host")
+
+	if pajbotHost == "" {
+		s.ChannelMessageSend(m.ChannelID, "This server does not have a pajbot host configured. Use the `$configure value` command to configure the `pajbot_host` key")
+		return pkg.CommandResultFullCooldown
+	}
+
 	if len(parts) < 2 {
 		s.ChannelMessageSend(m.ChannelID, "You need to provide a twitch username. usage: `$points <username>`")
 		return
@@ -44,7 +52,14 @@ func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []
 
 	target := parts[1]
 
-	resp, err := http.Get(fmt.Sprintf("https://forsen.tv/api/v1/users/%s?user_input=true", url.PathEscape(target)))
+	apiURL := url.URL{
+		Scheme:   "https",
+		Host:     pajbotHost,
+		Path:     fmt.Sprintf("api/v1/users/%s", target),
+		RawQuery: "user_input=true",
+	}
+
+	resp, err := http.Get(apiURL.String())
 	if err != nil {
 		s.ChannelMessageSend(m.ChannelID, "There was an error getting that user's data: "+err.Error())
 		return
