0e18f609b5f35aaa7eda46e1a4304ca33b729230
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-03-14 14:41:16 +0100

    Add "advanced delete" filter

--- internal/filter/filters.go
+++ internal/filter/filters.go
@@ -2,6 +2,8 @@ package filter
 
 import (
 	"context"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 type Mute struct {
@@ -26,6 +28,22 @@ type Delete struct {
 
 func (f *Delete) Check(ctx context.Context, message Message) (actions []Action, stop bool) {
 	if f.Checker(message.DiscordMessage.Content) {
+		return returnActions(ctx, []Action{
+			{
+				Delete: true,
+			},
+		}), true
+	}
+
+	return nil, false
+}
+
+type DeleteAdvanced struct {
+	Checker func(*discordgo.Message) bool
+}
+
+func (f *DeleteAdvanced) Check(ctx context.Context, message Message) (actions []Action, stop bool) {
+	if f.Checker(message.DiscordMessage) {
 		return returnActions(ctx, []Action{
 			{
 				Delete: true,
