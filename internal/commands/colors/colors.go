0bacb66ab700e1b52b0b6e09c3aaa2ca56857ca1
theyugin <theyugin@gmail.com>
2023-02-24 17:39:53 +0500

    feat: add colors to $colors command

--- internal/commands/colors/colors.go
+++ internal/commands/colors/colors.go
@@ -1,13 +1,12 @@
 package roleinfo
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
 	"github.com/pajbot/basecommand"
 	"github.com/pajbot/pajbot2-discord/pkg"
 	"github.com/pajbot/pajbot2-discord/pkg/commands"
 	"github.com/pajbot/pajbot2-discord/pkg/utils"
+	"strings"
 )
 
 func init() {
@@ -33,7 +32,7 @@ func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []
 	colorRoles := utils.ColorRoles(s, m.GuildID)
 	var roleNames []string
 	for _, role := range colorRoles {
-		roleNames = append(roleNames, role.Name)
+		roleNames = append(roleNames, role.Mention())
 	}
 
 	if len(roleNames) == 0 {
@@ -41,7 +40,10 @@ func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []
 		return
 	}
 
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("These are the available colors: %s", roleNames))
+	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+		Title:       "These are the available colors:",
+		Description: strings.Join(roleNames, " "),
+	})
 	return pkg.CommandResultUserCooldown
 }
 
