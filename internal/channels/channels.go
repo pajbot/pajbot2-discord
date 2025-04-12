b1d96012822568e34a9b1f57800cf3572370df5a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-04-12 14:40:36 +0200

    migrate channel configuration to its own slash command

--- internal/channels/channels.go
+++ internal/channels/channels.go
@@ -1,24 +1,81 @@
 package channels
 
 import (
+	"database/sql"
 	"fmt"
+	"iter"
+	"maps"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/pajbot/pajbot2-discord/internal/serverconfig"
 )
 
+type ChannelData struct {
+	Name        string
+	DisplayName string
+	Description string
+}
+
 var (
-	validChannelRoles = map[string]bool{
-		"action-log":          true,
-		"moderation-action":   true,
-		"weeb-channel":        true,
-		"system-messages":     true,
-		"connection-updates":  true,
-		"manual-verification": true,
+	validChannels = map[string]*ChannelData{
+		"action-log": {
+			Name:        "action-log",
+			DisplayName: "Action log",
+			Description: "",
+		},
+		"moderation-action": {
+			Name:        "moderation-action",
+			DisplayName: "Moderation action",
+			Description: "",
+		},
+		"weeb-channel": {
+			Name:        "weeb-channel",
+			DisplayName: "Weeb channel",
+			Description: "",
+		},
+		"system-messages": {
+			Name:        "system-messages",
+			DisplayName: "System messages",
+			Description: "",
+		},
+		"connection-updates": {
+			Name:        "connection-updates",
+			DisplayName: "Connection updates",
+			Description: "",
+		},
+		"manual-verification": {
+			Name:        "manual-verification",
+			DisplayName: "Manual verification",
+			Description: "",
+		},
 	}
 )
 
+func List() iter.Seq[*ChannelData] {
+	return maps.Values(validChannels)
+}
+
+func Choices() []*discordgo.ApplicationCommandOptionChoice {
+	choices := []*discordgo.ApplicationCommandOptionChoice{}
+
+	for channelKey, channelData := range validChannels {
+		choices = append(choices, &discordgo.ApplicationCommandOptionChoice{
+			Name:  channelData.DisplayName,
+			Value: channelKey,
+		})
+	}
+
+	return choices
+}
+
+func init() {
+	if len(validChannels) > 24 {
+		panic("Too many channels, things will break")
+	}
+}
+
 func ValidRole(channelRole string) (ok bool) {
-	_, ok = validChannelRoles[channelRole]
+	_, ok = validChannels[channelRole]
 	return
 }
 
@@ -46,4 +103,28 @@ func Get(guildID, channelType string, fallbackChannelTypes ...string) string {
 	}
 
 	return ""
+}
+
+func Set(sqlClient *sql.DB, guildID, botChannel, discordChannelID string) error {
+	if !ValidRole(botChannel) {
+		return fmt.Errorf("invalid bot channel: %s", botChannel)
+	}
+
+	if discordChannelID == "" {
+		return fmt.Errorf("missing discord channel ID")
+	}
+
+	key := fmt.Sprintf("channel:%s", botChannel)
+
+	return serverconfig.Save(sqlClient, guildID, key, discordChannelID)
+}
+
+func Clear(sqlClient *sql.DB, guildID, botChannel string) error {
+	if !ValidRole(botChannel) {
+		return fmt.Errorf("invalid bot channel: %s", botChannel)
+	}
+
+	key := fmt.Sprintf("channel:%s", botChannel)
+
+	return serverconfig.Remove(sqlClient, guildID, key)
 }
--- internal/commands/configure/configure.go
+++ internal/commands/configure/configure.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/pajbot/basecommand"
-	"github.com/pajbot/pajbot2-discord/internal/channels"
 	"github.com/pajbot/pajbot2-discord/internal/serverconfig"
 	"github.com/pajbot/pajbot2-discord/pkg"
 	"github.com/pajbot/pajbot2-discord/pkg/commands"
@@ -260,7 +259,7 @@ func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []
 		return
 	}
 
-	var configType, key, value string
+	var configType string
 	configType = parts[0]
 
 	// config type decides how we read the value
@@ -276,53 +275,8 @@ func (c *Command) Run(s *discordgo.Session, m *discordgo.MessageCreate, parts []
 		c.configureTwitter(s, m, parts)
 
 	case "channel":
-		const usage = "usage: $configure channel CHANNEL_ROLE VALUE(here/reset/get)"
-		if len(parts) < 3 {
-			s.ChannelMessageSend(m.ChannelID, usage)
-			return
-		}
-		key = parts[1]
-		value = parts[2]
-
-		if !channels.ValidRole(key) {
-			s.ChannelMessageSend(m.ChannelID, "Invalid key argument. "+usage)
-			return
-		}
-
-		key = configType + ":" + key
-
-		oldChannelID := serverconfig.Get(m.GuildID, key)
-
-		var newChannelID string
-
-		switch value {
-		case "here":
-			s.ChannelMessageSend(m.ChannelID, "channel here. old: "+oldChannelID)
-			newChannelID = m.ChannelID
-		case "reset":
-			s.ChannelMessageSend(m.ChannelID, "channel reset. old: "+oldChannelID)
-			newChannelID = ""
-		case "get":
-			s.ChannelMessageSend(m.ChannelID, "Current: "+oldChannelID+". <#"+oldChannelID+">")
-			return
-		default:
-			s.ChannelMessageSend(m.ChannelID, usage)
-			return
-		}
-
-		if newChannelID != "" {
-			err := serverconfig.Save(commands.SQLClient, m.GuildID, key, newChannelID)
-			if err != nil {
-				log.Println("SQL Error in set:", err)
-				return
-			}
-		} else {
-			err := serverconfig.Remove(commands.SQLClient, m.GuildID, key)
-			if err != nil {
-				log.Println("SQL Error in remove:", err)
-				return
-			}
-		}
+		const usage = "use the /channel command instead (admin only)"
+		s.ChannelMessageSend(m.ChannelID, usage)
 
 	case "role":
 		const usage = "use the /role command instead (admin only)"
