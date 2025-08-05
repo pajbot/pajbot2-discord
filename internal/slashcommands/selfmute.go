381942890f18dd7a3ba2cff5d9ce30719861c21c
Jer <58816840+JeremyEspresso@users.noreply.github.com>
2025-08-06 01:43:41 +0300

    Update selfmute command
    - rename selfmute -> focus
    - add IsMuted check
    - force choices by utilizing Application Command Choices
    - Send to action-log on successful self-mute

--- internal/slashcommands/selfmute.go
+++ internal/slashcommands/selfmute.go
@@ -4,21 +4,49 @@ import (
 	"fmt"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/pajbot/pajbot2-discord/internal/commands/mute"
+	"github.com/pajbot/pajbot2-discord/internal/channels"
+	"github.com/pajbot/pajbot2-discord/internal/mute"
+	"github.com/pajbot/pajbot2-discord/pkg/utils"
 )
 
 func init() {
 	cmd := &SlashCommand{
-		name: "selfmute",
+		name: "focus",
 
 		command: &discordgo.ApplicationCommand{
-			Description: "Zip your own mouth",
+			Description: "🧘 Mute yourself to enter peace mode. Silence distractions, clear your mind.",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
 					Name:        "duration",
 					Description: "Mute duration",
 					Required:    true,
+					Choices: []*discordgo.ApplicationCommandOptionChoice{
+						{
+							Name:  "1 Hour",
+							Value: "1h",
+						},
+						{
+							Name:  "2 Hours",
+							Value: "2h",
+						},
+						{
+							Name:  "4 Hours",
+							Value: "4h",
+						},
+						{
+							Name:  "8 Hours",
+							Value: "8h",
+						},
+						{
+							Name:  "16 Hours",
+							Value: "16h",
+						},
+						{
+							Name:  "24 Hours",
+							Value: "1d",
+						},
+					},
 				},
 			},
 		},
@@ -41,11 +69,33 @@ func init() {
 				return
 			}
 
+			muted, err := mute.IsUserMuted(sqlClient, i.GuildID, executingUser.ID)
+			if err != nil {
+				fmt.Println("Error checking user mute:", err)
+
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "mute check failed",
+					},
+				})
+				return
+			}
+
+			if muted {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You are already self-muted dummy, no takesies backsies",
+					},
+				})
+				return
+			}
+
 			options := i.ApplicationCommandData().Options
 			muteDuration := options[1].StringValue()
-			muteReason := "Self Mute"
 
-			if message, err := mute.Execute(s, i.GuildID, s.State.User, executingUser, muteDuration, muteReason); err != nil {
+			if _, err := mute.MuteUser(sqlClient, s, i.GuildID, s.State.User, executingUser, muteDuration, mute.SelfMuteReason); err != nil {
 				fmt.Println("Error executing self-mute:", err)
 				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 					Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -58,9 +108,18 @@ func init() {
 				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 					Type: discordgo.InteractionResponseChannelMessageWithSource,
 					Data: &discordgo.InteractionResponseData{
-						Content: message,
+						Content: "Inner peace obtained for " + muteDuration,
 					},
 				})
+
+				const resultFormat = "%s self-muted for %s"
+				message := fmt.Sprintf(resultFormat, utils.MentionUser(s, i.GuildID, executingUser), muteDuration)
+
+				targetChannel := channels.Get(i.GuildID, "action-log")
+				if targetChannel != "" {
+					// Announce self-mute in action-log channel
+					s.ChannelMessageSend(targetChannel, message)
+				}
 			}
 		},
 	}
